f49685cfb45033ef709fbc93b6abdb2aa71a0f5e
Matt Duffield <duffieldml@gmail.com>
2020-12-13 13:31:26 -0500

    Fixed lower and uppercase options

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ var passwordChoices;
 
 
 //Arrays of available choices for creating passwords
-var lowerCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var upperCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "#", "$", "%", "&", "'", ")", "(", "*", "+", ",", "-", "/", ":", ";", "<", "=", ">", "?", "@", "]", "[", "^", "_", ".", "}", "{", "|", "~", "`"];
 
